2cd300ffd31ec9ae513d8f6ccb4b1c3b9c7f2f64
Zinput <jasonzheng2004@gmail.com>
2024-04-07 21:49:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,155 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { 
+  Container,
+  AppBar,
+  Toolbar,
+  Box,
+  Typography,
+  Button,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import MenuIcon from '@mui/icons-material/Menu';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { useState } from 'react';
+import * as React from 'react';
+
+function FormDialog({open, handleClose}) {
+  return (
+    <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+  );
+}
 
 function App() {
+
+  function createData(title, description, deadline, priority, isComplete, action) {
+    return { title, description, deadline, priority, isComplete, action };
+  }
+
+  const [tasks, setTasks] = useState([{}]);
+
+  function Banner() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    return (
+      <React.Fragment>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <MenuIcon />
+              <Typography align="center" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                FRAMEWORKS
+              </Typography>
+              <Button variant="contained" onClick={handleClickOpen}><AddCircleIcon /> Add</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <FormDialog open={open} handleClose={handleClose} />
+      </React.Fragment>
+    );
+  }
+  
+  function TaskTable() {
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Title</TableCell>
+              <TableCell align="center">Description</TableCell>
+              <TableCell align="center">Deadline</TableCell>
+              <TableCell align="center">Priority</TableCell>
+              <TableCell align="center">Is Complete</TableCell>
+              <TableCell align="center">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tasks.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell align="center" component="th" scope="row">{row.title}</TableCell>
+                <TableCell align="center">{row.description}</TableCell>
+                <TableCell align="center">{row.deadline}</TableCell>
+                <TableCell align="center">{row.priority}</TableCell>
+                <TableCell align="center">{row.isComplete}</TableCell>
+                <TableCell align="center">{row.action}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Banner />
+      <TaskTable />
+    </Container>
   );
 }
 
