27ec33190fc64a98d902854659dc37bc9acf76be
Zinput <jasonzheng2004@gmail.com>
2024-04-08 22:07:29 -0400

    sync

--- src/App.js
+++ src/App.js
@@ -23,67 +23,132 @@ import {
   DialogActions,
   DialogContent,
   DialogContentText,
-  DialogTitle
+  DialogTitle,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Stack,
+  Checkbox,
+  Snackbar,
+  Alert
 } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import { DatePicker } from '@mui/x-date-pickers';
 import MenuIcon from '@mui/icons-material/Menu';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import BlockIcon from '@mui/icons-material/Block';
 import { useState } from 'react';
 import * as React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
-function FormDialog({open, handleClose}) {
+
+function FormDialog({open, handleClose, isEdit, handleAdd}) {
+
   return (
     <Dialog
         open={open}
         onClose={handleClose}
+        fullWidth
+        maxWidth="xs"
         PaperProps={{
           component: 'form',
           onSubmit: (event) => {
             event.preventDefault();
             const formData = new FormData(event.currentTarget);
             const formJson = Object.fromEntries(formData.entries());
-            const email = formJson.email;
-            console.log(email);
+            handleAdd({
+              title: formJson.title,
+              description: formJson.description,
+              priority: formJson.priority,
+              deadline: formJson.deadline,
+              isComplete: false
+            });
             handleClose();
           },
         }}
       >
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>
+          {isEdit 
+            ? "Edit Task" 
+            : <Stack alignItems="center" direction="row">
+                <AddCircleIcon />
+                <Typography>Add Task</Typography>
+              </Stack>}
+        </DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
           <TextField
             autoFocus
             required
-            margin="dense"
-            id="name"
-            name="email"
-            label="Email Address"
-            type="email"
+            margin="normal"
+            id="title"
+            name="title"
+            label="Title"
+            type="required"
             fullWidth
-            variant="standard"
+            variant="outlined"
           />
+          <TextField
+            autoFocus
+            required
+            margin="normal"
+            id="description"
+            name="description"
+            label="Description"
+            type="required"
+            fullWidth
+            variant="outlined"
+          />
+          <DatePicker 
+            autoFocus
+            id="deadline"
+            name="deadline"
+            label="Deadline"
+            variant="outlined"
+            sx={{width: "100%", my: "1rem"}}
+          />
+          <FormControl>
+            <FormLabel id="priority-label">Priority</FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="priority-label"
+              name="priority"
+              defaultValue="low"
+            >
+              <FormControlLabel value="low" control={<Radio />} label="Low" />
+              <FormControlLabel value="med" control={<Radio />} label="Med" />
+              <FormControlLabel value="high" control={<Radio />} label="High" />
+            </RadioGroup>
+          </FormControl>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Subscribe</Button>
+          <Button variant="contained" type="submit"><AddCircleIcon />ADD</Button>
+          <Button variant="contained" color="error" onClick={handleClose}><BlockIcon /> CANCEL</Button>
         </DialogActions>
       </Dialog>
   );
 }
 
 function App() {
-
   function createData(title, description, deadline, priority, isComplete, action) {
     return { title, description, deadline, priority, isComplete, action };
   }
 
-  const [tasks, setTasks] = useState([{}]);
+  const [tasks, setTasks] = useState([
+    {
+      title: "groceries",
+      description: "ice cream",
+      priority: "high",
+      deadline: "04/28/2024",
+      isComplete: false
+    }
+  ]);
 
   function Banner() {
     const [open, setOpen] = React.useState(false);
+    const [isEdit, setIsEdit] = React.useState(false);
 
     const handleClickOpen = () => {
       setOpen(true);
@@ -93,20 +158,44 @@ function App() {
       setOpen(false);
     };
 
+    const handleEdit = () => {
+      setIsEdit(true);
+    };
+
+    const handleAdd = ({task}) => {
+      setTasks(
+        [
+          ...tasks,
+          {task}
+        ]
+      )
+      return (
+        <Snackbar open={true} autoHideDuration={6000}>
+          <Alert
+            
+            severity="success"
+            variant="filled"
+            sx={{ width: '100%' }}
+          >
+            This is a success Alert inside a Snackbar!
+          </Alert>
+        </Snackbar>
+      );
+    };
+
     return (
       <React.Fragment>
         <Box sx={{ flexGrow: 1 }}>
           <AppBar position="static">
             <Toolbar>
-              <MenuIcon />
               <Typography align="center" variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                FRAMEWORKS
+                <MenuIcon />FRAMEWORKS
               </Typography>
               <Button variant="contained" onClick={handleClickOpen}><AddCircleIcon /> Add</Button>
             </Toolbar>
           </AppBar>
         </Box>
-        <FormDialog open={open} handleClose={handleClose} />
+        <FormDialog open={open} handleClose={handleClose} handleAdd={handleAdd} />
       </React.Fragment>
     );
   }
@@ -128,15 +217,25 @@ function App() {
           <TableBody>
             {tasks.map((row) => (
               <TableRow
-                key={row.name}
+                key={row.title}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell align="center" component="th" scope="row">{row.title}</TableCell>
                 <TableCell align="center">{row.description}</TableCell>
                 <TableCell align="center">{row.deadline}</TableCell>
                 <TableCell align="center">{row.priority}</TableCell>
-                <TableCell align="center">{row.isComplete}</TableCell>
-                <TableCell align="center">{row.action}</TableCell>
+                <TableCell align="center">
+                  {<Checkbox
+                    onClick={() => {
+                      row.isComplete = !row.isComplete
+                    }}
+                  />}
+                </TableCell>
+                <TableCell align="center">
+                  {
+                    row.isComplete ? "yes" : "no"
+                  }
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -146,11 +245,13 @@ function App() {
   }
 
   return (
-    <Container>
-      <Banner />
-      <TaskTable />
-    </Container>
-  );
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Container>
+        <Banner />
+        <TaskTable />
+      </Container>
+    </LocalizationProvider>
+  );  
 }
 
 export default App;
